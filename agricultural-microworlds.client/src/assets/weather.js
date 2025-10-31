567a7642cf7a86b2cfe7afdd2813136b5c721dc4
DakotaDenton <95587912+DakotaDenton@users.noreply.github.com>
2025-10-31 16:06:35 +0000

    Dynamic weather and dropdown for svg

--- agricultural-microworlds.client/src/assets/weather.js
+++ agricultural-microworlds.client/src/assets/weather.js
@@ -1,5 +1,22 @@
 // src/assets/weather.js
 //import to index
+
+export async function fetchKansasDegreeDays({
+  station = "ASHL",
+  base = 50,
+  start = "2025-04-01",
+  end = "2025-10-31",
+}) {
+  const url = `https://mesonet.k-state.edu/rest/degreedays/?station=${station}&base=${base}&start=${start}&end=${end}`;
+
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("Failed to fetch from Kansas Mesonet");
+
+  const data = await res.json();
+  return data;
+}
+
+
 const weatherData = [
   { LOCATION: "Manhattan", TEMP2MMIN: 10.4, TEMP2MMAX: 11.57 },
   { LOCATION: "Manhattan", TEMP2MMIN: 10.71, TEMP2MMAX: 12.42 },
