ff09fccf26b1c95e50b32e1beae5086a6d557ab0
DakotaDenton <95587912+DakotaDenton@users.noreply.github.com>
2025-12-01 16:19:49 +0000

    added vehicle states start

---
+++ agricultural-microworlds.client/src/SimulationState.js
@@ -0,0 +1,43 @@
+// SimulationState.js
+// A lightweight container for the data + functions your Vehicle States need.
+// You can expand or redirect these to your existing Simulation class.
+
+// SimulationState.js
+export default class SimulationState {
+    constructor(machine) {
+        this.machine = machine;
+
+        this.isMoving = false;
+        this.isHarvesting = false;
+        this.isSeeding = false;
+    }
+
+    async moveForward(duration) {
+        console.log("Moving forward for", duration);
+        this.isMoving = true;
+        await new Promise(r => setTimeout(r, duration * 100));
+        this.isMoving = false;
+    }
+
+    async turnXDegrees(degrees) {
+        console.log("Turning", degrees, "degrees");
+        await new Promise(r => setTimeout(r, Math.abs(degrees) * 10));
+    }
+
+    async waitXWeeks(weeks) {
+        console.log("Waiting for", weeks, "weeks");
+        await new Promise(r => setTimeout(r, weeks * 50));
+    }
+
+    toggleHarvesting(on) {
+        this.isHarvesting = on;
+        console.log("Harvesting:", on);
+    }
+
+    toggleSeeding(on) {
+        this.isSeeding = on;
+        console.log("Seeding:", on);
+    }
+}
+
+    export const simulation = new SimulationState(null)
---
+++ agricultural-microworlds.client/src/VehicleStates/BaseState.js
@@ -0,0 +1,30 @@
+// BaseState.js
+
+import { simulation } from "../SimulationState.js";
+
+export default class BaseState {
+    constructor(sim = simulation) {
+        // reference to the SimulationState instance
+        this.sim = sim;
+    }
+
+    // called when entering this state
+    enter(payload) {
+        // optional payload, default is undefined
+        return payload
+    }
+
+    // called on each update/tick
+    update(dt) {
+        // dt is the delta time (optional)
+        return dt
+
+        //can do change then return this.sim
+        //return this.sim;
+    }
+
+    // called when exiting this state
+    exit() {
+        // cleanup or reset logic
+    }
+}
