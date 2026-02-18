96098287c0fbb33f68eebaa0b12cc02221491780
cordan231 <142618197+cordan231@users.noreply.github.com>
2026-02-18 18:42:24 +0000

    FieldTile stuff

--- agricultural-microworlds.client/src/Simulation/SimManagers/FieldTileSimManager.js
+++ agricultural-microworlds.client/src/Simulation/SimManagers/FieldTileSimManager.js
@@ -1,13 +1,19 @@
-import FieldTileState from "../../States/StateClasses/FieldTileState";
-import SimManager from "../SimManager";
+// import FieldTileState from "../../States/StateClasses/FieldTileState";
+// import SimManager from "../SimManager";
 
-export default class FieldTileSimManager extends SimManager {
-  // update(deltaTime, oldState, newState)
-  // {
-  //     if (deltaTime <= 0) return;
-  //     const cropState = oldState.cropState;
-  //     const waterLevel = oldState.waterLevel;
-  //     const minerals = oldState.minerals;
-  //     const fieldTileState = new FieldTileState();
-  // }
-}
+// export default class FieldTileSimManager extends SimManager {
+//   update(deltaTime, oldState, newState)
+//   {
+//       if (deltaTime <= 0) return;
+//       const cropState = oldState.cropState;
+//       const waterLevel = oldState.waterLevel;
+//       const minerals = oldState.minerals;
+
+//       if (!cropState || !waterLevel || !minerals) return;
+
+//       const fieldTileState = new FieldTileState();
+//   }
+// }
+// 
+// I think that this CropSimManager should be in charge of updating the cropstate and FieldTileSimManager should only be in charge of updating the fieldtilestate. Your code makes it to where it updates the cropstate. How do I integrate both?
+
--- agricultural-microworlds.client/src/States/StateClasses/FieldTileState.js
+++ agricultural-microworlds.client/src/States/StateClasses/FieldTileState.js
@@ -7,3 +7,4 @@ export default class FieldTileState {
     this.minerals = 1000;
   }
 }
+
