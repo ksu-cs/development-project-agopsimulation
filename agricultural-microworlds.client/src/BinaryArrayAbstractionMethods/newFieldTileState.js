a6735671835b3485108f4cac783e3510896a10aa
jseimKSU <143106047+jseimKSU@users.noreply.github.com>
2026-03-02 19:22:03 +0000

    added some helper methods, still thinking through how things should work

--- agricultural-microworlds.client/src/BinaryArrayAbstractionMethods/newFieldTileState.js
+++ agricultural-microworlds.client/src/BinaryArrayAbstractionMethods/newFieldTileState.js
@@ -15,8 +15,10 @@ export default class BitmapFieldState {
         this.key = fieldTileKey;
         this.fieldProps = {};
         Object.entries(fieldTileKey).forEach(([name, value]) => {
-            let buffer = new ArrayBuffer(value.size * rows * columns);
+            let buffer = new ArrayBuffer((value.size * rows * columns) + 1);
             let field = new Uint8Array(buffer);
+            // since max size of a the value size is 255
+            if(!(value.size < 1 || value.size > 255)) field[0] = value.size;
             this.fieldProps[name] = field;
         });
     }
@@ -26,6 +28,53 @@ export default class BitmapFieldState {
      * @param {FieldTileState} initialTileState The crop state values to initialize the field to
      */
     InitializeField(initialTileState){
-        
+        // goes through every buffer in the dictionary
+        this.fieldProps.forEach(([name, array]) => {
+            // traverses the data part of the array buffer value[0] is the amount of bytes per section
+            for(let i = 1; i < array.length; i+=array[0]){
+                this.#ChangeFieldTileVar(i, array[0], array, /* value to change to */);
+            }
+        });
+    }
+
+    /**
+     * Sets the value of the given variable name at the given x and y
+     * @param {string} name name of the variable to change
+     * @param {any} value the value to change the variable to
+     * @param {number} x horizontal distance from origin
+     * @param {number} y vertical distance from origin
+     */
+    set(name, value, x, y){
+        let nameBufferArray = this.fieldProps[name];
+        let size = nameBufferArray[0];
+        let i = this.#GetTileIndex(x, y, size);
+        this.#ChangeFieldTileVar(i, size, nameBufferArray, value);
+    }
+
+    /**
+     * Changes field tile variable to the 
+     * @param {*} i The index to start altering at in the array
+     * @param {*} size The size of the value
+     * @param {*} arr The array to alter the values of
+     * @param {any} value 
+     */
+    #ChangeFieldTileVar(i, size, arr, value){
+        // shift the bits of every 1 byte of the initial state value over and store it in the array
+        for(let j = 0; j < size; j++){
+            arr[i + j] = (value >> ((size - j + 1) * 8)) & 0xff;
+        }
+    }
+
+
+
+    /**
+     * Calculates the proper index value for a ArrayBuffer
+     * @param {number} x horizontal distance away from origin
+     * @param {number} y vertical distance away from origin
+     * @param {number} byteSize the amount of bytes the variable is
+     * @returns an index that represents the x and y vars given
+     */
+    #GetTileIndex(x, y, byteSize){
+        return (this.width * y + x) * byteSize;
     }
 }
