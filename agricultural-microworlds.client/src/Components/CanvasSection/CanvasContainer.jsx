ba1a479ce86dc6d71b981f483db45d86b511ca95
cordan231 <142618197+cordan231@users.noreply.github.com>
2026-03-05 18:39:33 +0000

    Camera stays on selected vehicle

--- agricultural-microworlds.client/src/Components/CanvasSection/CanvasContainer.jsx
+++ agricultural-microworlds.client/src/Components/CanvasSection/CanvasContainer.jsx
@@ -34,6 +34,9 @@ class CanvasContainer extends Component {
             <p className={styles.statText} id="rainText">
               Precipitation: 0.00
             </p>
+            <p className={styles.statText} id="activeVehicleText">
+              Active Vehicle: Harvester
+            </p>
           </div>
           <canvas
             id="gameCanvas"
