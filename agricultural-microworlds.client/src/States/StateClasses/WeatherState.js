849e22048bb403682e52baabdd48eacff1ff2ab2
MBCaseyKS <142626351+MBCaseyKS@users.noreply.github.com>
2026-03-25 17:50:55 +0000

    Night now fades based on time, increased time scale while waiting.

--- agricultural-microworlds.client/src/States/StateClasses/WeatherState.js
+++ agricultural-microworlds.client/src/States/StateClasses/WeatherState.js
@@ -7,6 +7,7 @@ export default class WeatherState {
     this.currentDayIndex = 0;
     this.timeAccumulator = 5; // Counts up to 24.0 (1 second = 1 hour)
     this.speedMultiplier = 1; // Controls simulation speed
+    this.isWaiting = false;
 
     // GDD Tracking
     this.cumulativeGDD = 0;
@@ -22,9 +23,19 @@ export default class WeatherState {
     s.currentDayIndex = this.currentDayIndex;
     s.timeAccumulator = this.timeAccumulator;
     s.speedMultiplier = this.speedMultiplier;
+    s.isWaiting = this.isWaiting;
 
     s.cumulativeGDD = this.cumulativeGDD;
     s.gddToApplyThisFrame = this.gddToApplyThisFrame;
     return s;
+  }
+
+  /**
+   * Gets the current speed multiplier
+   * @returns {number} The speed multi at which the game should run at.
+   */
+  getSpeedMultiplier() {
+    if (this.isWaiting) return this.speedMultiplier * 6.0;
+    return this.speedMultiplier;
   }
 }
