825fe1a90ee91a84895faab19017a6d81451ad73
DakotaDenton <95587912+DakotaDenton@users.noreply.github.com>
2026-04-22 10:38:43 -0500

    Added rain and watering functionality as well as stress from water (#65)
    * Added rain and watering functionality as well as stress from water
    
    * moved rain loop to cropsimmanager

--- agricultural-microworlds.client/src/States/StateClasses/ImplementState.js
+++ agricultural-microworlds.client/src/States/StateClasses/ImplementState.js
@@ -32,7 +32,7 @@ export default class ImplementState {
     this.isHarvestingOn = false;
     this.isSeedingOn = false;
     this.cropBeingPlanted = CROP_TYPES.WHEAT;
-
+    this.isWateringOn = false;
     this.yieldScore = 0;
     this.type = VEHICLES.HARVESTER;
   }
@@ -51,7 +51,7 @@ export default class ImplementState {
 
     newState.isMoving = this.isMoving;
     newState.isHarvestingOn = this.isHarvestingOn;
-
+    newState.isWateringOn = this.isWateringOn;
     newState.isSeedingOn = this.isSeedingOn;
 
     newState.cropBeingPlanted = this.cropBeingPlanted;
